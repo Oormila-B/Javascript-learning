7a59f9bbb09a55e949189cd7026b1e923dfafa1e
Oormila-B <oormilaoormila03@gmail.com>
2024-09-11 21:03:00 +0530

    Update Fundamentals 2.js

--- Fundamentals 2.js
+++ Fundamentals 2.js
@@ -64,3 +64,17 @@ const yearUntilRetirement = (birthYear,firstName) => {
 console.log(yearUntilRetirement(1991,'Oormila'));
 console.log(yearUntilRetirement(1980,'Yokesh'));
 
+//Functions calling other functions
+
+/*function cutFruitPieces(fruit){
+    return fruit*4;
+}
+function fruitProcessor(apples,oranges){
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    console.log(apples,oranges);
+    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2,3));*/
