83b3c3363fa2db2d96585716d8cacf80219342fd
Oormila B <oormilaoormila03@gmail.com>
2024-09-20 07:04:19 +0530

    Update Fundamentals 2.js

--- Fundamentals 2.js
+++ Fundamentals 2.js
@@ -189,5 +189,74 @@ console.log(age1,age2,age3,age4,age5);//47 70 -12530 17 16
 const ages=[calcAge(years[0]),calcAge(years[1]),calcAge(years[years.length-1])];
 console.log(ages);*/
 
+//Basic Array Operations 
+/*const friends =['Oormila','Yokesh'];
+friends.push('Arun');//add end of the array
+console.log(friends);
+friends.unshift('Bala');//Add element first in the array
+console.log(friends);
+//remove element 
+friends.pop();//remove last element //remove last element
+const popped=friends.pop();//show which element next popped
+console.log(popped);//Yokesh
+console.log(friends);//[ 'Bala', 'Oormila' ]
+
+//Remove first element from array
+
+friends.shift();//remove first element 
+console.log(friends);//Oormila//
+console.log(friends.indexOf('Oormila'));//0
+//To know elements in the array or not
+console.log(friends.includes('Oormila'));//True
+console.log(friends.includes('Arun'));//false*/
+
+//challenge 6
+//question
+/*CHALLENGE #2
+Steven wants you to improve his tip calculator, using the same rules as before — tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+Your tasks:
+
+Write a function calcTip that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from the first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+
+And now let's use arrays! So, create an array called bills containing the test data below.
+
+Create an array called tips containing the tip value for each bill, calculated from the function you created before.
+
+BONUS: Create an array totals containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555, and 44.*/
+
+//my answer
+/*const calcTip= function(bills){
+  if(bills>=50 && bills<=300){
+      return bills*0.15;
+  }
+  else {
+      return bills*0.20;
+  }
+}
+const bills=[125,555,44];
+const tips=[calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])];
+const totals=[bills[0]+tips[0] , bills[1]+tips[1],bills[2]+tips[2]];
+console.log(bills,tips,totals);*/
+
+//object 
+
+/*const oormiArray=[
+  'Oormila',
+  'developer',
+  2037-1991,
+  ['oormila','Yokesh']
+];
+
+const oormi={
+  firstName: 'Oormila',
+  lastName: 'Bala'
+  age: 2024-2002, // key : property
+  job: 'Developer',
+  friends:['oormila','yokesh']
+}*/
+
 
 
